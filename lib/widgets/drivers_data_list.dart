5b5b81a9b1d7cd7ea18d297a67c6921fa1853058
robertotonde <robertotonde316@gmail.com>
2024-01-29 18:09:40 +0300

    connect firebase

--- lib/widgets/drivers_data_list.dart
+++ lib/widgets/drivers_data_list.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:kp_admin_web/methods/common_methods.dart';
 
 class DriversDataList extends StatefulWidget {
   const DriversDataList({super.key});
@@ -8,8 +10,52 @@ class DriversDataList extends StatefulWidget {
 }
 
 class _DriversDataListState extends State<DriversDataList> {
+  final driversRecordsFromDatabase =
+      FirebaseDatabase.instance.ref().child("drivers");
+
+
+      CommonMethods cMethods = CommonMethods();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+        stream: driversRecordsFromDatabase.onValue,
+        builder: (BuildContext context, snapshotData) {
+          if (snapshotData.hasError) {
+            return const Center(
+              child: Text(
+                "ERROR FETHING DATA TRY TO RELOAD",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.pink),
+              ),
+            );
+          }
+          if (snapshotData.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          Map dataMap = snapshotData.data!.snapshot.value as Map;
+
+          List itemList = [];
+
+          dataMap.forEach((key, value) {
+            itemList.add({"key": key, ...value});
+          });
+
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: itemList.length,
+            itemBuilder: ((context, index) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                   cMethods.data(2, itemList[index]["id"])
+                ],
+              );
+            }),
+          );
+        });
   }
 }
